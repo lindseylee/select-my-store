88321c80ba820c024490609edc26e7ed7310e071
Lindsey Lee <lb.lee3@yahoo.com>
2014-08-15 16:07:04 -0500

    complete search results business logic

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,26 @@
 class ResultsController < ApplicationController
+
   def results
-    # @stores = StoreStyle.find(params[:id])
+    age = params[:age]
+    sex = params[:sex]
+    style = params[:style]
+    # @age = #write activerecord method here
+    # @sex = Style.where("sex = ?", [:sex] )
+    # @style = StoreStyle.where(:style_id => [1,2,3]
+
+    @store_results = Store.joins(:styles)
+    .where('stores.min <= 18 AND 18 <= stores.max')
+    .where('styles.sex = ?', "female")
+    .where('styles.id' => [3,4,6])
+    .uniq
   end
+
 end
 
 
 #write method that queries the database for age, sex, and styles
+
+@store_results = Store.includes(:styles)
+      .where('stores.min > 18 AND 18 < stores.max')
+      .where('styles.sex = ?', 'female')
+      .where('styles.id => [2, 3, 4]')
