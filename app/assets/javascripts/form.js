71457b93479c1c572d576f8631ded10b9823a908
Lindsey Lee <lb.lee3@yahoo.com>
2014-08-18 15:37:28 -0500

    implement isotope with styles based on gender

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,12 +1,53 @@
-<script language="javascript" type="text/javascript">
 
-$( "selectsex" )
-  .change(function () {
-    var str = "";
-    $( "sex option:selected" ).each(function() {
-      str += $( this ).text() + " ";
+// var input = $( "form input:radio" )
+//   $(".femalebutton").click(function(){
+//   $(Style.sex["male"]).hide();
+//   });
+//   $(".femalebutton").click(function(){
+//   $(Style.sex["female"]).show();
+//   });
+//   $(".malebutton").click(function(){
+//   $(Style.sex["female"]).hife();
+//   });
+//   $(".malebutton").click(function(){
+//   $(Style.sex["male"]).show();
+//   });
+// });
+
+//         $('input[type="radio"]').click(function(){
+//             if($(this).attr("value")=="female"){
+//                 $(Style.sex["male"]).hide();
+//                 $(Style.sex["female"]).show();
+//             }
+//             if($(this).attr("value")=="male"){
+//                 $(Style.sex["female"]).hide();
+//                 $(Style.sex["male"]).show();
+//             }
+//         });
+//     });
+// </script>
+
+$(document).ready(function() {
+    var $isotopeContainer = $('.styles-container');
+    $isotopeContainer.isotope({
+        itemSelector: '.style-button',
+        layoutMode: 'masonry'
     });
-    $( "div" ).text( str );
-  })
-  .change();
-</script>
+
+    $('.radiobuttons input[type="radio"]').on('change', function(){
+
+        if(this.checked && $(this).attr("value") == "female") {
+            $isotopeContainer.isotope({
+                filter: '.female'
+            });
+        } else {
+            $isotopeContainer.isotope({
+                filter: '.male'
+            });
+        }
+    });
+})
+
+ 
+
+
