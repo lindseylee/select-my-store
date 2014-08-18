9f5d86cd0bb3772ec39068143f0023bd0f314196
Lindsey Lee <lb.lee3@yahoo.com>
2014-08-18 15:40:45 -0500

    update store and social media images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,12 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def new 
-    @user = User.new
-    @sex = StoreStyle.sex
-    @age = Age.new
-    @style1 = Style.new
-    @style2 = Style.new
-    @style3 = Style.new
-  end
+  # def new 
+  #   @user = User.new
+  #   @sex = StoreStyle.sex
+  #   @age = Age.new
+  #   @style1 = Style.new
+  #   @style2 = Style.new
+  #   @style3 = Style.new
+  # end
 end
